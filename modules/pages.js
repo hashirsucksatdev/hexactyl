cfb88d0288538f7af1097dc5d53dd9e1110ec6ca
Hashir <mostwantedhashir+1@gmail.com>
2025-03-24 13:09:20 +0500

    hexactyl

--- modules/pages.js
+++ modules/pages.js
@@ -7,10 +7,10 @@ const fetch = require("node-fetch");
 const arciotext = require("../handlers/afk.js");
 
 /* Ensure platform release target is met */
-const heliactylModule = { "name": "Pages", "target_platform": "10.0.0" };
+const hexactylModule = { "name": "Pages", "target_platform": "10.0.0" };
 
 /* Module */
-module.exports.heliactylModule = heliactylModule;
+module.exports.hexactylModule = hexactylModule;
 module.exports.load = async function (app, db) {
   app.all("/", async (req, res) => {
     try {
